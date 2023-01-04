bde2dadd3849d4ab036f01f233400a7276ac4245
Eric Snyder <guideon72@hotmail.com>
2023-01-04 12:59:35 -0800

    Create minimal .pdf for each file in the invoices directory

--- invoicer.py
+++ invoicer.py
@@ -3,18 +3,29 @@ import pandas as pd
 from fpdf import FPDF
 from pathlib import Path
 
-# Read all files in directory into a Python list
-base_path = Path(Path.cwd() / "data" / "invoices").glob("*.xlsx")
-invoices = [f for f in base_path if f.is_file()]
 
-# Convert each file to a Pandas dataframe
-for invoice in invoices:
-    df = pd.read_excel(invoice, sheet_name="Sheet 1")
-    print(df)
+def mk_pdf(data, f_name, f_date):
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice #: {f_name}")
+    pdf.ln()
+    pdf.cell(w=50, h=8, txt=f"Invoice date: {f_date}")
+    pdf.output(f"data/output/{data}.pdf")
+
+
+def convert_invoices():
+    # Read all files in directory into a Python list
+    base_path = Path(Path.cwd() / "data" / "invoices").glob("*.xlsx")
+    invoices = [f for f in base_path if f.is_file()]
+    for invoice in invoices:
+        iv_name, iv_date = invoice.stem.split("-")
+        df = pd.read_excel(invoice, sheet_name="Sheet 1")
+        mk_pdf(invoice.name, iv_name, iv_date)
 
 
 def main():
-    pass
+    convert_invoices()
 
 
 if __name__ == "__main__":
